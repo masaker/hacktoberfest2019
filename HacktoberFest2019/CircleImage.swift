63e603e57c289ad504cabb39769e80679ad191d2
Sanel Zsivics <zsivics.sanel@gmail.com>
2019-10-31 17:52:38 +0100

    image view added

---
+++ HacktoberFest2019/CircleImage.swift
@@ -0,0 +1,21 @@
+//
+//  CircleImage.swift
+//  HacktoberFest2019
+//
+//  Created by Sanel Zsivics on 10/31/19.
+//  Copyright © 2019 Sanel Zsivics. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
