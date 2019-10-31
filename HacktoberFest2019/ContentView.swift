6520dac682739c9ea5018cc02dcc03902e4e4b79
Sanel Zsivics <zsivics.sanel@gmail.com>
2019-10-31 17:03:42 +0100

    Initial Commit

---
+++ HacktoberFest2019/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HacktoberFest2019
+//
+//  Created by Sanel Zsivics on 10/31/19.
+//  Copyright © 2019 Sanel Zsivics. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
