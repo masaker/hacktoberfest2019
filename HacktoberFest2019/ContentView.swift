065998144d35a9dc41861ca19ff1da9e3031a88e
Sanel Zsivics <zsivics.sanel@gmail.com>
2019-10-31 17:33:31 +0100

    subtitle added

--- HacktoberFest2019/ContentView.swift
+++ HacktoberFest2019/ContentView.swift
@@ -10,9 +10,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Turtle Rock")
+        VStack {
+            Text("Turtle Rock")
                 .font(.title)
-                .foregroundColor(.green)
+            Text("Joshua Tree National Park")
+        }
     }
 }
 
