c7c77832b571a30c8502d1171c47c67d719ca219
Sanel Zsivics <zsivics.sanel@gmail.com>
2019-10-31 17:38:06 +0100

    added alignment and padding, added location with spacer

--- HacktoberFest2019/ContentView.swift
+++ HacktoberFest2019/ContentView.swift
@@ -8,14 +8,21 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
             Text("Turtle Rock")
                 .font(.title)
-            Text("Joshua Tree National Park")
-                .font(.subheadline)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
         }
+        .padding()
     }
 }
 
